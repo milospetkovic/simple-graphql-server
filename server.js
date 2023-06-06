dd2db3627fa1cf196a6b9aae49668736424ae95e
Miloš Petković <elitasoft.web@gmail.com>
2023-06-06 22:43:33 +0200

    refs #3
    * a new book added..
    
    @0.25h

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const books = [
   { id: 9, name: 'Learn to say No', authorId: 3 },
   { id: 10, name: 'Gone with a storm', authorId: 3 },
   { id: 11, name: 'Na Drini cuprija', authorId: 4 },
+  { id: 12, name: 'Prokleta avlija', authorId: 4 },
 ]
 
 const BookType = new GraphQLObjectType({
